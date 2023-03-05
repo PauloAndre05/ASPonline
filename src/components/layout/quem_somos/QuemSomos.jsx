539d7f6fabcedc1bfff1a4685a72d474c7714f92
PauloAndre05 <pauloandrepaulinho05@gmail.com>
2023-03-05 21:41:05 +0000

    Preparando a sessão Quem Somos

---
+++ src/components/layout/quem_somos/QuemSomos.jsx
@@ -0,0 +1,11 @@
+import styles from './QuemSomos.module.css'
+function QuemSomos() {
+    return(
+        <main>
+            <section>
+                
+            </section>
+        </main>
+    )
+}
+export{ QuemSomos }
